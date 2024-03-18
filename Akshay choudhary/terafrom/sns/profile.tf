c5e53db54b3f88f9ef2541653a285d8b87333dc9
Akshay-7693032755 <159227011+Akshay-7693032755@users.noreply.github.com>
2024-03-18 08:16:59 +0530

    Delete Akshay choudhary directory

--- Akshay choudhary/terafrom/sns/profile.tf
+++
@@ -1,5 +0,0 @@
-provider "aws" {
-  region     = "ap-northeast-2"
-  access_key = "AKIATGNV2F7TH6ESAYPY"
-  secret_key = "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"
-}
--- Akshay choudhary/terafrom/vpc/output,.tf
+++
@@ -1,9 +0,0 @@
-output   "vpc_id" {
-    description   =   "VPC_ID"
-    value   =aws_vps.this_vpc.id
-}
-
-output   "public_sub_cidr_block" {
-    description  =   "publicsubID"
-     value  = aws_subnet.public_sub.id
-}
